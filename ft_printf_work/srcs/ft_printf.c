4cb1ffa222cd7ecdbf6342be7be6e371df4ae433
sapril <mokijoy@gmail.com>
2019-10-30 11:33:31 +0300

    123

---
+++ ft_printf_work/srcs/ft_printf.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sapril <sapril@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/17 13:26:42 by sapril            #+#    #+#             */
+/*   Updated: 2019/10/24 18:33:53 by sapril           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+//
+
+void ft_printf(char *input, ...)
+{
+	t_param *param;
+
+	param = (t_param*)ft_memalloc(sizeof(t_param));
+	param->str = input;
+	param->padding = 0;
+	param->precision = 0;
+	param->iter = 0;
+	va_start(param->args, input);
+	while (param->str[param->iter])
+	{
+		if (param->str[param->iter] == '%')
+		{
+			param->arg_value = va_arg(param->args, void *);
+			param->iter++;
+			parse_flag(param);
+		}
+		else
+			write(1, &param->str[param->iter], 1);
+		param->iter++;
+	}
+
+}
+
